b4879ce52aaa229bde2b59c987bc2ac1d347bfaa
DesirePro <59990384+DeftSolutions-dev@users.noreply.github.com>
2023-01-10 13:12:33 -0500

    Update SystemInfo.cs

--- Checker/Targets/Info/SystemInfo.cs
+++ Checker/Targets/Info/SystemInfo.cs
@@ -10,28 +10,23 @@ namespace Checker
     { 
         public static string username = Environment.UserName; 
         public static string compname = Environment.MachineName;
-        public static string GetSystemVersion() // Получение версии виндовс
-        {
-            return GetWindowsVersionName() + " " + GetBitVersion();
-        }
+        public static string GetSystemVersion() => return GetWindowsVersionName() + " " + GetBitVersion();// Получение версии виндовс
         public static string GetWindowsVersionName()// Версия виндовс
         {
-            string sData = "Unknown System";
+            var sData = "Unknown System";
             try
             {
-                using (ManagementObjectSearcher mSearcher = new ManagementObjectSearcher(@"root\CIMV2", " SELECT * FROM win32_operatingsystem"))
+                using (var mSearcher = new ManagementObjectSearcher(@"root\CIMV2", " SELECT * FROM win32_operatingsystem"))
                 {
-                    foreach (ManagementObject tObj in mSearcher.Get())
+                    foreach (var tObj in mSearcher.Get())
                         sData = Convert.ToString(tObj["Name"]);
                     sData = sData.Split(new char[] { '|' })[0];
-                    int iLen = sData.Split(new char[] { ' ' })[0].Length;
+                    var iLen = sData.Split(new char[] { ' ' })[0].Length;
                     sData = sData.Substring(iLen).TrimStart().TrimEnd();
                 }
             }
-            catch 
-            {
-                return "Error";
-            }
+            catch  
+                return "Error"; 
             return sData;
         }
         private static string GetBitVersion() // Получение битности
@@ -46,22 +41,20 @@ namespace Checker
                 else
                     return "(64 Bit)";
             }
-            catch 
-            {
-                return "Error";
-            } 
+            catch  
+                return "Error"; 
         }
         
         public static string ScreenMetrics() // Получение разрешение экрана
         {
-            Rectangle bounds = System.Windows.Forms.Screen.GetBounds(Point.Empty);
-            int width = bounds.Width;
-            int height = bounds.Height;
+            var bounds = System.Windows.Forms.Screen.GetBounds(Point.Empty);
+            var width = bounds.Width;
+            var height = bounds.Height;
             return width + "x" + height;
         }
         public static string GetHWID() // Получаем HWID
         {
-            string HWID = string.Empty;
+            var HWID = string.Empty;
             HWID = Registry.LocalMachine.OpenSubKey(@"SYSTEM\\CurrentControlSet\\Control\\SystemInformation")
                     .GetValue("ComputerHardwareId")
                     .ToString(); 
@@ -71,133 +64,115 @@ namespace Checker
         {
             try
             {
-                string GPU = string.Empty;
-                ManagementObjectSearcher mSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_DiskDrive");
-                foreach (ManagementObject mObject in mSearcher.Get())
+                var GPU = string.Empty;
+                var mSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_DiskDrive");
+                foreach (var mObject in mSearcher.Get())
                 {
-                    for (int i = 0; i < 10; i++)
-                    {
-                            GPU = mObject["Name"] + $"\n     ID: {mObject["SerialNumber"]}";
-                    }
+                    for (int i = 0; i < 10; i++) 
+                         GPU = mObject["Name"] + $"\n     ID: {mObject["SerialNumber"]}"; 
                 }
                 return GPU;
             }
-            catch
-            {
-                return "Error";
-            }
+            catch 
+                return "Error"; 
         }
         public static string GetCPU() // Получение инфу процессора
         {
             try
             {
-                string CPU = string.Empty;
-                ManagementObjectSearcher mSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_Processor");
-                foreach (ManagementObject mObject in mSearcher.Get())
+                var CPU = string.Empty;
+                var mSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_Processor");
+                foreach (var mObject in mSearcher.Get())
                     CPU = mObject["Name"] + $" [{mObject["NumberOfCores"]}/{mObject["NumberOfLogicalProcessors"]}]" + $"\n     ID: {mObject["ProcessorId"]}";
                 return CPU;
             }
-            catch 
-            {
-                return "Error";
-            }
+            catch  
+                return "Error"; 
         }
         public static string GetRAM() // Получаем инфу RAM
         {
             try
             {
-                string RAM = string.Empty;
-                int RamAmount = 0;
-                ManagementObjectSearcher mSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_ComputerSystem");
-                foreach (ManagementObject mObject in mSearcher.Get())
+                var RAM = string.Empty;
+                var RamAmount = 0;
+                var mSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_ComputerSystem");
+                foreach (var mObject in mSearcher.Get())
                 {
-                    double Bytes = Convert.ToDouble(mObject["TotalPhysicalMemory"]);
+                    var Bytes = Convert.ToDouble(mObject["TotalPhysicalMemory"]);
                     RamAmount = (int)(Bytes / 1048576) - 1; 
                     break;
                 }
-                ManagementObjectSearcher a = new ManagementObjectSearcher("SELECT * FROM Win32_OperatingSystem");
-                foreach (ManagementObject mObject in a.Get())
+                var a = new ManagementObjectSearcher("SELECT * FROM Win32_OperatingSystem");
+                foreach (var mObject in a.Get())
                 { 
                     RAM = mObject["SerialNumber"].ToString();
                     break;
                 }
                 return RamAmount.ToString() + "MB" + $"\n     ID: {RAM}";
             }
-            catch
-            {
-                return "Error";
-            }
+            catch 
+                return "Error"; 
         } 
         public static string GetGPU() // Получаем инфу видеокарты
         {
             try
             {
-                string GPU = string.Empty;
-                int GPUAmount = 0;
-                ManagementObjectSearcher mSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_VideoController");
-                foreach (ManagementObject mObject in mSearcher.Get())
+                var GPU = string.Empty;
+                var GPUAmount = 0;
+                var mSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_VideoController");
+                foreach (var mObject in mSearcher.Get())
                 {
-                    double Bytes = Convert.ToDouble(mObject["AdapterRam"]);
+                    var Bytes = Convert.ToDouble(mObject["AdapterRam"]);
                     GPUAmount = (int)(Bytes / 1048576) - 1;
                     GPU = mObject["Name"] + $" [{GPUAmount}MB]";
                 } 
                 return GPU;
             }
-            catch 
-            {
-                return "Error";
-            }
+            catch  
+                return "Error"; 
         }
         public static string GetSMBIOS() // Получаем инфу об материнки
         {
             try
             {
-                string GPU = string.Empty; 
-                ManagementObjectSearcher mSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_BaseBoard");
-                foreach (ManagementObject mObject in mSearcher.Get())
+                var GPU = string.Empty; 
+                var mSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_BaseBoard");
+                foreach (var mObject in mSearcher.Get())
                 { 
                     GPU = mObject["Product"] + $"\n     ID: {mObject["SerialNumber"]}";
                 }
                 return GPU;
             }
-            catch 
-            {
-                return "Error";
-            }
+            catch  
+                return "Error"; 
         }
         public static string GetMAC() // Получаем инфу об MAC Address
         {
             try
             {
-                string MAC = string.Empty;
-                ManagementObjectSearcher mSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_NetworkAdapter");
-                foreach (ManagementObject mObject in mSearcher.Get())
+                var MAC = string.Empty;
+                var mSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_NetworkAdapter");
+                foreach (var mObject in mSearcher.Get())
                 {
                     MAC = mObject["Name"] + $"\n     ID: {mObject["MACAddress"]}";
                 }
                 return MAC;
             }
-            catch 
-            {
-                return "Error";
-            }
+            catch  
+                return "Error"; 
         }
         public static string GetPhysicalMemory() // Получаем инфу об PhysicalMemory
         {
             try
             {
-                string PhysicalMemory = string.Empty;
-                ManagementObjectSearcher mSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_PhysicalMemory");
-                foreach (ManagementObject mObject in mSearcher.Get())
-                {
-                    PhysicalMemory = mObject["PartNumber"].ToString();
-                }
+                var PhysicalMemory = string.Empty;
+                var mSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_PhysicalMemory");
+                foreach (var mObject in mSearcher.Get()) 
+                    PhysicalMemory = mObject["PartNumber"].ToString(); 
                 return PhysicalMemory;
             }
-            catch
-            {
-                return "Error";
-            }
+            catch 
+                return "Error"; 
         }
     }
 }
